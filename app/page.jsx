feafe29da84f834448283c91bcaebf97dfdf6889
10sandeep <sandeepnayak1424@gmail.com>
2025-01-25 22:25:35 +0530

    updated

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,13 @@
-
-
 import HeroSection from "@/components/Hero/Hero";
 import NavBar from "@/components/NavBar/NavBar";
 import React from "react";
 
 const page = () => {
+
   return (
     <div>
-     <NavBar/>
-   <HeroSection/>
+      <NavBar />
+      <HeroSection />
     </div>
   );
 };
